2150d897f836505e90ab2e0f1d4c065c668c369a
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-11-26 15:31:18 +0200

    Imported resources from previous project - aiming to upgrade with authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import GameList from "./components/GameList/GameList";
+import GameCreate from "./components/GameCreate/GameCreate";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import GameDetails from "./components/GameDetails/GameDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="box">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/games/:gameId" element={<GameDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/GameList/GameListItem/GameListItem.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+
+export default function GameListItem ({
+    title, category, imageUrl, _id
+}) {
+    return(
+        <div className="allGames">
+        <div className="allGames-info">
+          <img src={imageUrl} />
+          <h6>{category}</h6>
+          <h2>{title}</h2>
+          <Link to={`/games/${_id}`} className="details-button">Details</Link>
+        </div>
+      </div>
+    )
+}
